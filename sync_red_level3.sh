8c34737d7e9fbccde5c25a607f736e7c1a8415c5
goodboy-gg <Rosawoldemariam@yahoo.co.uk>
2026-08-15 15:57:44 +0100

    Fix Level 3 sync helper to restore files directly from GitHub

--- sync_red_level3.sh
+++ sync_red_level3.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 TARGET="release-1.0.0-test/Fidel-Crossword-Ethiopia-Release-1.0.0/app/lib/screens"
-cp app/lib/screens/levels_screen.dart "$TARGET/levels_screen.dart"
-cp app/lib/screens/challenge_crossword_screen.dart "$TARGET/challenge_crossword_screen.dart"
+mkdir -p "$TARGET"
+git show origin/learn-fidel-safe-build:app/lib/screens/levels_screen.dart > "$TARGET/levels_screen.dart"
+git show origin/learn-fidel-safe-build:app/lib/screens/challenge_crossword_screen.dart > "$TARGET/challenge_crossword_screen.dart"
 echo "RED LEVEL 3 READY"
